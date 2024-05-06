24d74aebe9410807b0d6d64874d6275b0053c520
Azheemah awoyemi <1488336@stu.ucenmanchester.ac.uk>
2024-05-06 16:08:23 +0100

    Added main class

---
+++ src/com.vetrecords/Main.java
@@ -0,0 +1,10 @@
+package com.vetrecords;
+
+import com.vetrecords.ui.MainFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+    }
+}
